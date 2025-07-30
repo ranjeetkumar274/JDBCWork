00409ea8774b37e0d65bcdab54ed990842d65f9f
ranjeetkumar274 <2023073045@mmmut.ac.in>
2025-07-30 17:08:54 +0530

    Day1 as Setting UP

---
+++ src/com/ashu/InsertBook.java
@@ -0,0 +1,33 @@
+ package com.ashu;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement; 
+
+public class InsertBook {
+	private static final String INSERT_SQL = "INSERT INTO BOOKS VALUE(101,'Java',1000)";
+	public static void main(String[] args) throws Exception {
+		
+// Step-1 Load Drivers	
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Class Found!");
+		
+//	Step-2 Get DB Connection	
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/advjdb","root","@Dune274");
+		System.out.println("Connection Established!");
+		
+//	Step-3 Create A Statement
+		Statement stmt = con.createStatement();
+		System.out.println("Statement created.");
+		
+//	Step-4 Execute the query
+		int rowsEffected = stmt.executeUpdate(INSERT_SQL);     //this is used for DML operations
+		
+//	Step-5 Process the result
+		System.out.println("Record Inserted count : "+rowsEffected);
+		
+//	Step-6 Close Connection
+		con.close();
+		System.out.println("Connection Closed.");
+	}
+}
